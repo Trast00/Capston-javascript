623500f3dd0ff6f0923cf0ed87e04871602f9d4e
Allassane <dickoallassane1997@gmail.com>
2023-01-03 12:28:20 +0100

    Add display method to ListMovies.js to display dynamicaly the number of movies

--- src/modules/ListMovies.js
+++ src/modules/ListMovies.js
@@ -8,7 +8,44 @@ export default class ListMovies {
 
   /* Display (Load dynnamically) a limited number of movies */
   display = (limit) => {
-    
+    const listDisplayed = this.list.slice(this.idLastDisplayed, this.idLastDisplayed + limit)
+    this.idLastDisplayed += limit
+    console.log(this.idLastDisplayed)
+    listDisplayed.forEach(movie => {
+      //dynamic load of list of movies
+      const liMovies = document.createElement('li')
+      liMovies.classList.add('flex-center', 'movie')
+      liMovies.id = `movie-${movie.id}`
+
+      const wrapper = document.createElement('div')
+      const img = document.createElement('img')
+      img.src = movie.url
+      img.alt = `Movies ${movie.name} image`
+
+      const content = document.createElement('div')
+      content.classList.add('flex-center', 'movie-content')
+      const h3 = document.createElement('h3')
+      h3.textContent = movie.name
+      const divLikes = document.createElement('div')
+      divLikes.classList.add('flex-center', 'likes')
+      const iconLike = document.createElement('i')
+      iconLike.classList.add('fa-reguar', 'fa-heart')
+      const pLike = document.createElement('p')
+      pLike.textContent = "3 Like"
+
+      const btnShowComment = document.createElement('button')
+      btnShowComment.classList.add('btn-show-comment')
+      btnShowComment.textContent = 'Comments'
+
+      /* List of append */
+      divLikes.append(iconLike, pLike)
+      content.append(h3, divLikes)
+      wrapper.append(img, content)
+      liMovies.append(wrapper, btnShowComment)
+
+      const ulListMovies = document.getElementById('list-movies')
+      ulListMovies.append(liMovies)
+    });
   }
 
   /* Get list of movies with a GET request to the API:  */
