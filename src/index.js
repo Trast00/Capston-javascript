df69d8fc75a64a8fc936d2eee7d77235b0ec1270
Donald Akite <donaldakite27@gmail.com>
2023-01-03 13:33:56 +0000

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import ListMovies from './modules/ListMovies';
+import ListMovies from './modules/ListMovies.js';
 import './style.css';
 
-const ulListMovies = document.getElementById('list-movies')
+const ulListMovies = document.getElementById('list-movies');
 
-const listMovies = new ListMovies()
+const listMovies = new ListMovies();
 
 window.addEventListener('load', async () => {
-  await listMovies.getList()
-  listMovies.display(10)
-})
+  await listMovies.getList();
+  listMovies.display(10);
+});
